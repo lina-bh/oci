26a928755a400b039c482f4b38cfd195a5df1c3c
lina-bh <36717206+lina-bh@users.noreply.github.com>
2026-09-13 22:04:26 +0100

    set up external secrets

--- vault.tf
+++ vault.tf
@@ -4,6 +4,24 @@ resource "oci_kms_vault" "vault" {
   vault_type     = "DEFAULT"
 }
 
+resource "local_file" "vault" {
+  filename        = "${path.root}/k8s/infrastructure/external-secrets/external-secrets.io_ClusterSecretStore_oci.yaml"
+  file_permission = "0644"
+  content         = <<-EOT
+apiVersion: external-secrets.io/v1
+kind: ClusterSecretStore
+metadata:
+  namespace: external-secrets
+  name: oci
+spec:
+  provider:
+    oracle:
+      vault: ${oci_kms_vault.vault.id}
+      region: uk-london-1
+      principalType: InstancePrincipal
+EOT
+}
+
 resource "oci_kms_key" "primary" {
   compartment_id      = data.oci_identity_availability_domains.ad.compartment_id
   management_endpoint = oci_kms_vault.vault.management_endpoint
@@ -13,4 +31,47 @@ resource "oci_kms_key" "primary" {
     length    = 32
   }
   protection_mode = "SOFTWARE"
+}
+
+resource "tailscale_oauth_client" "operator" {
+  description = "oke"
+  scopes      = ["devices:core", "auth_keys", "services"]
+  tags        = ["tag:k8s-operator"]
+}
+
+resource "oci_vault_secret" "tailscale_operator" {
+  compartment_id = oci_kms_vault.vault.compartment_id
+  vault_id       = oci_kms_vault.vault.id
+  key_id         = oci_kms_key.primary.id
+  secret_name    = "tailscale_operator"
+
+  secret_content {
+    content_type = "BASE64"
+    content = base64encode(jsonencode({
+      client_id     = tailscale_oauth_client.operator.id
+      client_secret = tailscale_oauth_client.operator.key
+    }))
+  }
+}
+
+resource "local_file" "operator-oauth" {
+  filename        = "${path.root}/k8s/infrastructure/tailscale/external-secrets.io_ExternalSecret_operator-oauth.yaml"
+  file_permission = "0644"
+  content         = <<-EOT
+---
+apiVersion: external-secrets.io/v1
+kind: ExternalSecret
+metadata:
+  namespace: tailscale
+  name: operator-oauth
+spec:
+  secretStoreRef:
+    kind: ClusterSecretStore
+    name: oci
+  target:
+    name: operator-oauth
+  dataFrom:
+  - extract:
+      key: ${oci_vault_secret.tailscale_operator.secret_name}
+EOT
 }
