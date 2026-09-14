1719c774de1cf629ef1f3170e414198ffb2c9d82
lina-bh <36717206+lina-bh@users.noreply.github.com>
2026-09-14 15:21:06 +0100

    rationalise resource names

--- vault.tf
+++ vault.tf
@@ -5,7 +5,7 @@ resource "oci_kms_vault" "vault" {
 }
 
 resource "local_file" "vault" {
-  filename        = "${path.root}/k8s/infrastructure/configs/external-secrets/external-secrets.io_ClusterSecretStore_oci.yaml"
+  filename        = "${path.root}/k8s/infrastructure/configs/external-secrets/clustersecretstore_oci.yaml"
   file_permission = "0644"
   content         = <<-EOT
 apiVersion: external-secrets.io/v1
@@ -54,7 +54,7 @@ resource "oci_vault_secret" "tailscale_operator" {
 }
 
 resource "local_file" "operator-oauth" {
-  filename        = "${path.root}/k8s/infrastructure/controllers/tailscale-operator/externalsecret.external-secrets.io_operator-oauth.yaml"
+  filename        = "${path.root}/k8s/infrastructure/controllers/tailscale-operator/externalsecret_operator-oauth.yaml"
   file_permission = "0644"
   content         = <<-EOT
 ---
